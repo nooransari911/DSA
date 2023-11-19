c0afa77e7821941e2fa4d1bdaf94f7c73a209d4a
Noor Ansari <mohammadna71@gmail.com>
2023-11-20 00:40:48 +0530

    added function for balancing AVL

--- git_push.sh
+++ git_push.sh
@@ -2,5 +2,5 @@ ssh -T git@github.com
 git add .
 git remote set-url origin git@github.com:nooransari911/DSA.git
 read commit_message
-git commit -m commit_message
+git commit -m "$commit_message"
 git push -u origin main
