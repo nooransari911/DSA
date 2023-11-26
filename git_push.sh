7813e1c041e45f1d0c154841c0b7d74bd3ad7363
Noor Ansari <mohammadna71@gmail.com>
2023-11-26 13:26:38 +0530

    Updated git_push

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,7 @@
 ssh -T git@github.com
 git add .
 git remote set-url origin git@github.com:nooransari911/DSA.git
+echo "Enter commit message:"
 read commit_message
 git commit -m "$commit_message"
 git push -u origin main
